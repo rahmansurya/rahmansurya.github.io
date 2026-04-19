8ee61a7bd0b15af007559185f7e1a9ca8ffb1886
PEMUDA TERSESAT <rahmansurya@gmail.com>
2026-04-19 17:04:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function runScript() {
     document.getElementById('output').innerHTML = `
-        <h2>✅ BERHASIL!</h2>
+        <h2>BERHASIL!</h2>
         <p><strong>Waktu:</strong> ${new Date().toLocaleString('id-ID')}</p>
         <p><strong>Hosting:</strong> GitHub Pages + Raw URL</p>
         <p><strong>URL Raw JS:</strong> https://raw.githubusercontent.com/rahmansurya/tersesat.github.io/main/script.js</p>
